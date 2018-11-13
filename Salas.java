1d9086f0170faa18c6b85b5a021993bb88f20a7d
Vítor de Melo Calhau <42739346+VMCalhau@users.noreply.github.com>
2018-11-13 13:53:46 -0200

    Add files via upload

--- Salas.java
+++ Salas.java
@@ -2,31 +2,18 @@ import java.io.*;
 import javax.swing.*;
 import java.net.*;
 
-public class Salas extends Thread
+public class Salas
 {
 	private ArrayList<Sala> salas;
-	/*protected ServerSocket server;
-	protected Socket conexao;*/
+	private int quantidade = 0;
 
-	/*public Salas (Socket c)
-	{
-		this.conexao = c;
+	public Salas(){
+		salas = new ArrayList<Sala>();
 	}
 
-	public static void main(String[] args)
-	{
-		try
-		{
-			server = new ServerSocket(12345);
+	public Sala[] getSalas() {
+		return this.salas.toArray();
+	}
 
-			for (;;)
-			{
-			    conexao = server.accept();
-				Thread t = new Salas(conexao);
-				t.start();
-			}
-		}
-		catch (Exception e)
-		{}
-	}*/
+
 }
